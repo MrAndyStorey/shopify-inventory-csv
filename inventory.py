7907bb7b9f29e6b236d9bb1658b0ef13b2840819
Andy Storey <betterseo@hotmail.co.uk>
2021-05-09 16:22:35 +0100

    First version that simply conntects to Shopify.

--- inventory.py
+++ inventory.py
@@ -17,24 +17,40 @@ parser = argparse.ArgumentParser(description='')
 parser.add_argument("--out", default="inventory.csv", type=str, help="Output filename - default = inventory.csv.")
 args = parser.parse_args()
 
-
 # Load the environment variables from .env.
 load_dotenv()
-# os.environ or os.getenv
+
+# Private applications authenticate with Shopify through basic HTTP authentication, 
+# using the URL format https://{apikey}:{password}@{hostname}/admin/api/{version}/{resource}.json
+apiKey = os.getenv("SHOPIFY_API_KEY")
+apiPasswd = os.getenv("SHOPIFY_API_SECRET")
+apiURL = os.getenv("SHOPIFY_URL")
+apiVersion = os.getenv("SHOPIFY_API_VERSION")
+baseURL = "https://{}:{}@{}/admin/api/{}/".format(apiKey,apiPasswd,apiURL,apiVersion)
 
 if __name__ == '__main__':
 
   # Open the output file and loop through each keyword one by one
-  with open(args.output, 'w+', encoding='utf-8') as output_file:
+  with open(args.out, 'w+', encoding='utf-8') as output_file:
     csv_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     csv_writer.writerow(['Product', 'Product Type', 'Unit Cost', 'Quantity', 'Line Total'])
 
+    # Connect to Shopify, and get a list of products.
+    with shopify.Session.temp(apiURL, apiVersion, apiPasswd):
+      print(shopify.GraphQL().execute("{ shop { name id } }"))
+
     # Showing a progress bar to the user.
     with Bar('Processing products.', max=100) as bar:
-
-
+      
+      productName = ""
+      productType = ""
+      productCost = 0
+      productQty = 0
+      productTotal = 0
 
       # Write the data for this row to the csv file.
-      csv_writer.writerow([keyword, builtResults, builtFree, builtPaid, builtAvgRating, builtAvgReviews, builtTopX.rstrip('|')])
+      csv_writer.writerow([productName, productType, productCost, productQty, productTotal])
       bar.next()
-  output_file.close()
+
+    # Close the output file
+    output_file.close()
